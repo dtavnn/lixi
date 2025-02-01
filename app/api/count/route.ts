bb9715f1a278df8fa7f1ef68fa4da2679413db5f
Anhdt <dtavnn@gmail.com>
2025-02-01 19:48:26 +0700

    Update route.ts

--- app/api/count/route.ts
+++ app/api/count/route.ts
@@ -4,11 +4,13 @@ export const runtime = "edge";
 
 export async function GET() {
   try {
+    console.log("NEXT_PUBLIC_API1:", process.env.NEXT_PUBLIC_API);
     const count = await fetch(
       `${process.env.NEXT_PUBLIC_API ?? ""}/count/tuimu`
     ).then((res) => res.json());
     return NextResponse.json({ ...count });
   } catch (error) {
+     console.log("NEXT_PUBLIC_API2:", process.env.NEXT_PUBLIC_API);
     console.error("Error", error);
     return NextResponse.json({ message: "Error" }, { status: 500 });
   }
